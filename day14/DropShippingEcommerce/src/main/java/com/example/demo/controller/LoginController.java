0b9814b0492818a1d12f9683d8192b041a788f97
Mohammed shoiab MA <119106244+mxdshoiab@users.noreply.github.com>
2023-10-06 15:11:02 +0530

    Add files via upload

---
+++ day14/DropShippingEcommerce/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+		info =@Info(
+				title = "App Title",
+				version = "1.1.2",
+				description = "App Description",
+				contact = @Contact(
+						name = "Author Name",
+						email = "Author Email"
+						)
+				)
+		)
+
+
+public class LoginController{
+}
