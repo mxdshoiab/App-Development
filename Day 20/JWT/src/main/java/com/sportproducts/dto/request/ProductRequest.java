4e2ffb51c34852f19e2ea8fbd3168e2a107f56a3
Mohammed shoiab MA <119106244+mxdshoiab@users.noreply.github.com>
2023-10-13 14:58:17 +0530

    Add files via upload

---
+++ Day 20/JWT/src/main/java/com/sportproducts/dto/request/ProductRequest.java
@@ -0,0 +1,16 @@
+package com.sportproducts.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    private String productName;
+    private float productPrice;
+    private String description;
+}
